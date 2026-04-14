774fe01248758ad147a0d1ca40477f5251ea0de5
tarm4442-spec <tarm4442@gmail.com>
2026-04-14 22:57:15 +0000

    Update settings.js

--- function/settings/settings.js
+++ function/settings/settings.js
@@ -14,12 +14,12 @@ global.setting = {
  }
 
 global.info = {
- nomerbot : '212605784394',
- pairingNumber : '212605784394',
+ nomerbot : '22242203253',
+ pairingNumber : '22242203253',
  figlet: 'silanalite', // create a start console display
- nomorwa : '212605784394',
- nameown : 'NOUREDDINE',
- nomerown : '212717457920',
+ nomorwa : '22242203253',
+ nameown : 'MED TAR',
+ nomerown : '967735686159',
  packname : 'sticker by ',
  author : 'SILANA AI',
  namebot : '乂 SILANA AI',
